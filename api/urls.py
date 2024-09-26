daf04e28567ee0cc7cee4a8d7dd2feea58b4d3a9
burkay <bpehlivan235@gmail.com>
2024-09-27 00:27:08 +0300

    register appointment viewset to urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
+from appointments.api.viewsets import AppointmentViewSet
 from doctors.api.viewsets import DoctorViewSet
 
 urlpatterns = []
@@ -8,6 +9,7 @@ urlpatterns = []
 router = DefaultRouter(trailing_slash=True)
 
 router.register("doctors", DoctorViewSet, basename="doctors")
+router.register("appointments", AppointmentViewSet, basename="appointments")
 
 
 urlpatterns += router.urls
