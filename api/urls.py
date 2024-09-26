29f8bdb8122f0581c720cb7ee3aabb62ece7116e
burkay <bpehlivan235@gmail.com>
2024-09-26 18:46:36 +0300

    add api urls to project

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
 
+from doctors.api.viewsets import DoctorViewSet
 
 urlpatterns = []
 
 
 router = DefaultRouter(trailing_slash=True)
+
+router.register("doctors", DoctorViewSet, basename="doctors")
 
 
 urlpatterns += router.urls
